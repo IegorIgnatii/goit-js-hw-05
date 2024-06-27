2a9fa97e5190a8c0d180d3c0ac966320bd196651
IegorIgnatii <egor.ignatiy@gmail.com>
2024-06-27 15:34:18 +0300

    T2

--- js/task-1.js
+++ js/task-1.js
@@ -1,23 +1,53 @@
-function isEnoughCapacity(products, containerSize) {
-  let totalSum = 0;
+// Напиши стрілочну функцію getUserNames(users), яка прийматиме один параметр users — масив об’єктів користувачів. Функція має повертати масив імен усіх користувачів (властивість name) із масиву users.
 
-  for (const key in products) {
-    if (products.hasOwnProperty(key)) {
-      totalSum += products[key];
-    }
-  }
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її викликів.
 
-  if (totalSum <= containerSize) {
-    return true;
-  }
+// const planets = ['Earth', 'Mars', 'Venus', 'Jupiter'];
 
-  return false;
-}
+// const planetsInUpperCase = planets.map(planet => planet.toUpperCase());
+// console.log(planetsInUpperCase); // ["EARTH", "MARS", "VENUS", "JUPITER"]
 
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+const getUserNames = users.map(user => user.name);
 
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+// const names = students.map(student => student.name);
 
-console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+console.log(getUserNames);
 
-console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+getUserNames([
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    balance: 2811,
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    balance: 3821,
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    balance: 3793,
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    balance: 2278,
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    balance: 3951,
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    balance: 1498,
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    balance: 2764,
+  },
+]);
+// ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
